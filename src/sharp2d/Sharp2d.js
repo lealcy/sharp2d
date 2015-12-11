07988e9ccfcdc35d2429c5aa08a461ee2a8cd6e6
Lealcy Tardelli Belegante <lealcy@gmail.com>
2015-12-11 18:21:31 -0200

    Initial commit
    Currently not working properly.

---
+++ src/sharp2d/Sharp2d.js
@@ -0,0 +1,20 @@
+"use strict";
+
+import "Mouse.js";
+import "Keyboard.js";
+import "Scene.js";
+
+export mouse;
+export keyboard;
+export defaultScene;
+
+export class Sharp2d {
+    constructor(canvasElement) {
+        // this.canvas = canvasElement;
+        // this.context = this.canvas.getContext("2d");
+        // mouse = new Mouse(this.canvas);
+        // keyboard = new Keyboard(this.canvas);
+        // defaultScene = new Scene();
+        // this.scene = defaultScene;
+    }
+}
---
+++ test/Game.js
@@ -0,0 +1,9 @@
+"use strict";
+
+import "../src/sharp2d/Sharp2d.js";
+
+export class Game extends Shard2d {
+    construct(canvasElement) {
+        super(canvasElement);
+    }
+}
