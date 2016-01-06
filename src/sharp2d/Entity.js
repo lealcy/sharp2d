7d4c3603f1deb9b97f6cb824b8b5db540d7c8c5d
Lealcy Tardelli Belegante <lealcy@gmail.com>
2016-01-05 22:11:00 -0200

    You can now scale an image in the x and y coordinates.

--- src/sharp2d/Entity.js
+++ src/sharp2d/Entity.js
@@ -9,6 +9,8 @@ class Entity {
         this._height = 0;
         this._visible = false;
         this._scale = 1.0;
+        this._xScale = 1.0;
+        this._yScale = 1.0;
     }
 
     start() {
@@ -29,12 +31,28 @@ class Entity {
     set y(value) { this._y = value; }
     get y() { return this._y; }
     set width(value) { this._width = value; }
-    get width() { return this._width * this._scale; }
+    get width() { return this._width * this._xScale; }
     set height(value) { this._height = value; }
-    get height() { return this._height * this._scale; }
+    get height() { return this._height * this._yScale; }
     set visible(value) { this._visible = value; }
     get visible() { return this._visible; }
-    set scale(value) { this._scale = value >= 0 ? value : this._scale; }
+    set scale(value) {
+        if (value >= 0) {
+            this._scale = this._xScale = this._yScale = value;
+        }
+    }
     get scale() { return this._scale; }
+    set xScale(value) {
+        if (value >= 0) {
+            this._xScale = value;
+        }
+    }
+    get xScale() { return this._xScale; }
+    set yScale(value) {
+        if (value >= 0) {
+            this._yScale = value;
+        }
+    }
+    get yScale() { return this._yScale; }
 
 }
