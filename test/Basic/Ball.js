c67787cbd792e11266966bd56793ed543a4367a1
Lealcy Tardelli Belegante <lealcy@gmail.com>
2016-02-02 19:41:00 -0200

    Updated test.

--- test/Basic/Ball.js
+++ test/Basic/Ball.js
@@ -8,20 +8,25 @@ class Ball extends Sprite {
         this.x = 100;
         this.y = 100;
         this.pivot = this.pivots.center;
+        this.mouse.ignoreOutEvent = true;
     }
 
     _update() {
+        if (this.mouse.leftClick) {
+            console.log("click");
+            var newBall = this.clone();
+            newBall.move(this.mouse.x, this.mouse.y);
+            this.parent.add(newBall);
+        }
+
         if (this.mouse.leftDown && !this.onTheMove && this.mouseOver) {
             this.originX = this.mouse.x - this.x;
             this.originY = this.mouse.y - this.y;
-            this.mouseOriginX = this.mouse.x;
-            this.mouseOriginY = this.mouse.y;
             this.onTheMove = true;
         }
 
         if (this.onTheMove) {
-            this.x = this.mouse.x - this.originX;
-            this.y = this.mouse.y - this.originY;
+            this.move(this.mouse.x - this.originX, this.mouse.y - this.originY);
         }
 
         if (this.mouse.leftUp) {
