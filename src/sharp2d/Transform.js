a66b7724cc261759ea6367b962ef63ff69cc830a
Lealcy Tardelli Belegante <lealcy@gmail.com>
2016-02-18 21:46:03 -0200

    Pivot + Position merged into this

--- src/sharp2d/Transform.js
+++ src/sharp2d/Transform.js
@@ -1,17 +1,18 @@
 "use strict";
 
 class Transform extends Component {
-    constructor(name, entity) {
-        super(name, entity);
+    constructor(name) {
+        super(name);
         this.defaults();
     }
 
     beforeUpdate() {
         this.game.drawSurface.saveContext();
+        this.game.drawSurface.translate(this._pivotX, this._pivotY);
         this.game.drawSurface.transform(this._scaleWidth, this._skewHorizontal,
             this._skewVertical, this._scaleHeight, this._translateX,
             this._translateY);
-        this.game.rotate(this._rotateAngle * Math.PI / 180);
+        this.game.drawSurface.rotate(this._rotateAngle * Math.PI / 180);
     }
 
     afterUpdate() {
@@ -19,6 +20,10 @@ class Transform extends Component {
     }
 
     defaults() {
+        this._x = 0;
+        this._y = 0;
+        this._width = 0;
+        this._height = 0;
         this._scaleWidth = 1.0;
         this._scaleHeight = 1.0;
         this._rotateAngle = 0.0;
@@ -26,6 +31,9 @@ class Transform extends Component {
         this._skewVertical = 0.0;
         this._translateX = 0;
         this._translateY = 0;
+        this._pivot = this.pivots.topLeft;
+        this._pivotX = 0;
+        this._pivotY = 0;
     }
 
     scale(width, height) {
@@ -43,6 +51,43 @@ class Transform extends Component {
     translate(x, y) {
         this._translateX = x;
         this._translateY = y;
+    }
+
+    position(x, y) {
+        this._x = x;
+        this._y = y;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    set x(value) {
+        this._x = value;
+    }
+
+    get y() {
+        return this._y;
+    }
+
+    set y(value) {
+        this._y = value;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    set width(value) {
+        this._width = value;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    set height(value) {
+        this._height = value;
     }
 
     get scaleWidth() {
@@ -69,7 +114,96 @@ class Transform extends Component {
         return this._rotateAngle;
     }
 
-    set rotate(angle) {
-        this._rotateAngle = angle;
+    set rotate(value) {
+        this._rotateAngle = value;
     }
+
+    get pivot() {
+        return this._pivot;
+    }
+
+    set pivot(value) {
+        this._pivot = value;
+        this._updatePivot();
+    }
+
+    get pivotX() {
+        return this._pivotX;
+    }
+
+    set pivotX(value) {
+        this._pivot = this.pivots.custom;
+        this._pivotX = value;
+    }
+
+    get pivotY() {
+        return this._pivotY;
+    }
+
+    set pivotY(value) {
+        this._pivot = this.pivots.custom;
+        this._pivotY = value;
+    }
+
+    _updatePivot() {
+        switch(this._pivot) {
+            case this.pivots.top:
+                this._pivotX = -(this._entity.width / 2);
+                this._pivotY = 0;
+                break;
+            case this.pivots.topRight:
+                this._pivotX = -this._entity.width;
+                this._pivotY = 0;
+                break;
+            case this.pivots.right:
+                this._pivotX = -this._entity.width;
+                this._pivotY = -(this._entity.height / 2);
+                break;
+            case this.pivots.bottomRight:
+                this._pivotX = -this._entity.width;
+                this._pivotY = -this._entity.height;
+                break;
+            case this.pivots.bottom:
+                this._pivotX = -(this._entity.width / 2);
+                this._pivotY = -this._entity.height;
+                break;
+            case this.pivots.bottomLeft:
+                this._pivotX = 0;
+                this._pivotY = -this._entity.height;
+                break;
+            case this.pivots.left:
+                this._pivotX = 0;
+                this._pivotY = -(this._entity.height / 2);
+                break;
+            case this.pivots.topLeft:
+                this._pivotX = 0;
+                this._pivotY = 0;
+                break;
+            case this.pivots.center:
+                this._pivotX = -(this._entity.width / 2);
+                this._pivotY = -(this._entity.height / 2);
+                break;
+            case this.pivots.custom:
+                break;
+            default:
+                this._x = 0;
+                this._y = 0;
+                break;
+        }
+    }
+
+
 }
+
+Transform.prototype.pivots = {
+    center: Symbol("center"),
+    top: Symbol("top"),
+    topRight: Symbol("topRight"),
+    right: Symbol("right"),
+    bottomRight: Symbol("bottomRight"),
+    bottom: Symbol("bottom"),
+    bottomLeft: Symbol("bottomLeft"),
+    left: Symbol("left"),
+    topLeft: Symbol("topLeft"),
+    custom: Symbol("custom"),
+};
