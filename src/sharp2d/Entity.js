3e862bb629719d00e6580b5fdeaab980ebc6a720
Lealcy Tardelli Belegante <lealcy@gmail.com>
2016-02-18 21:43:40 -0200

    Position and Pivot code transfered to Transform
    Better component handling;
    Composition merged into this;
    Better event support;

--- src/sharp2d/Entity.js
+++ src/sharp2d/Entity.js
@@ -4,13 +4,9 @@ class Entity extends BaseObject {
     constructor(name) {
         super(name);
         this._components = [];
-        this._x = 0;
-        this._y = 0;
-        this._width = 0;
-        this._height = 0;
-        this._transform = this._instanciateComponent(Transform,
-            this._name + ".transform");
-        this._pivot = this._instanciateComponent(Pivot, this._name + ".pivot");
+        this._children = [];
+        this._transform = new Transform(this._name + ".transform", this);
+        this._transform.entity = this;
     }
 
     start() {
@@ -21,47 +17,25 @@ class Entity extends BaseObject {
         // To be extended.
     }
 
-    addComponent(component, name) {
-        var newComponent = this._instanciateComponent(component, name);
-        this._components.push(newComponent);
-        return newComponent;
+    addComponent(component) {
+        this._components.push(component);
+        component.entity = this;
+        return component;
     }
 
-    position(x, y) {
-        this.x = x;
-        this.y = y;
+    addEntity(child) {
+        this._children.push(child);
     }
 
-    get x() {
-        return this._x;
-    }
-
-    set x(value) {
-        this._x = value;
-    }
-
-    get y() {
-        return this._y;
-    }
-
-    set y(value) {
-        this._y = value;
-    }
-
-    get width() {
-        return this._width;
-    }
-
-    set width(value) {
-        this._width = value;
-    }
-
-    get height() {
-        return this._height;
-    }
-
-    set height(value) {
-        this._height = value;
+    callEvent() {
+        if (this._enabled) {
+            this._transform.callEvent(...arguments);
+            this._components.forEach(
+                component => component.callEvent(...arguments)
+            );
+            this._children.forEach(child => child.callEvent(...arguments));
+            super.callEvent(...arguments);
+        }
     }
 
     get transform() {
@@ -70,36 +44,6 @@ class Entity extends BaseObject {
 
     set transform(value) {
         this._transform = value;
-    }
-
-    get pivot() {
-        return this._pivot;
-    }
-
-    _beforeStart() {
-        this._components.forEach(component => component.beforeStart());
-        return true;
-    }
-
-    _afterStart(game) {
-        this._components.forEach(component => component.afterStart());
-    }
-
-    _beforeUpdate(game) {
-        this._transform.beforeUpdate();
-        this._pivot.beforeUpdate();
-        this._components.forEach(component => component.beforeUpdate());
-        return true;
-    }
-
-    _afterUpdate(game) {
-        this._components.forEach(component => component.afterUpdate());
-        this._pivot.afterUpdate();
-        this._transform.afterUpdate();
-    }
-
-    _instanciateComponent(component, name) {
-        return new component(name, this);
     }
 
     /*    get mouseOver() {
