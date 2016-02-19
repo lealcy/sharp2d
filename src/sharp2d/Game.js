58674a049c7ca03e2dce71e5eebc8879db804513
Lealcy Tardelli Belegante <lealcy@gmail.com>
2016-02-19 09:20:26 -0200

    Game throws an animationFrame event instead of update
    Game can now be enabled and disabled safely.

--- src/sharp2d/Game.js
+++ src/sharp2d/Game.js
@@ -7,7 +7,7 @@ class Game extends BaseObject {
         this._drawSurface = new CanvasDrawSurface("defaultCanvas", canvasElement);
         this._mouse = new Mouse("defaultMouse", canvasElement);
         this._keyboard = new Keyboard("defaultKeyboard", canvasElement);
-        this._updateInterval = 1000 / 60; // 60 fps. Glorious PC Master Race.
+        this._refreshInterval = 1000 / 60; // 60 fps. Glorious PC Master Race.
         this._clearOnUpdate = true;
         this._world = new Entity("defaultComposition");
         this._started = false;
@@ -20,17 +20,24 @@ class Game extends BaseObject {
             this._requestAnimFrame = window.mozRequestAnimationFrame;
         } else {
             this._requestAnimFrame = (callback) => {
-                setTimeout(callback, this._updateInterval);
+                setTimeout(callback, this._refreshInterval);
             };
         }
     }
 
     start() {
-        this._mouse.start();
-        this._keyboard.start();
-        this._started = true;
-        this._world.callEvent("start");
-        this._update();
+        if (!this._started) {
+            this._started = true;
+            this._mouse.start();
+            this._keyboard.start();
+            this._world.callEvent("start");
+        }
+        this.enable;
+    }
+    
+    get enable() {
+        super.enable;
+        this._animationFrame();
     }
 
     get started() {
@@ -64,13 +71,13 @@ class Game extends BaseObject {
         this._clearOnUpdate = value;
     }
 
-    _update() {
+    _animationFrame() {
         if (this._enabled && this._started) {
-            this._requestAnimFrame.call(window, this._update.bind(this));
+            this._requestAnimFrame.call(window, this._animationFrame.bind(this));
             if (this._clearOnUpdate) {
                 this._drawSurface.clear();
             }
-            this._world.callEvent("update");
+            this._world.callEvent("animationFrame");
             this._mouse.update();
             this._keyboard.update();
         }
