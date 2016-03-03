9f66a0a247765d64dddd7f0808a3279cbd642877
Lealcy Tardelli Belegante <lealcy@gmail.com>
2016-03-02 21:32:56 -0300

    Adds terminalVelocity parameter

--- src/sharp2d/Components/Gravity.js
+++ src/sharp2d/Components/Gravity.js
@@ -1,14 +1,16 @@
 "use strict";
 
 class Gravity extends Component {
-    constructor(name, entity, acceleration) {
+    constructor(name, entity, acceleration, terminalVelocity) {
         super(name, entity);
-        this._acceleration = acceleration || 0.5;
-        this._vy = 0;
+        this._acceleration = acceleration || 1.1;
+        this._terminalVelocity = terminalVelocity || 14;
+        this._vy = this._acceleration;
     }
 
-    update() {
-        this._vy += this._acceleration;
-        this.entity.transform.y += this._vy;
+    beforeUpdate() {
+        this._vy *= this._acceleration;
+        this.entity.transform.y += this._vy < this._terminalVelocity ?
+            this._vy : this._terminalVelocity;
     }
 }
