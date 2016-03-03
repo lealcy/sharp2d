f1145db4c5a72f8094e8e57dccef24588cda26c3
Lealcy Tardelli Belegante <lealcy@gmail.com>
2016-03-02 21:37:19 -0300

    callEvent method transfered to Entity

--- src/sharp2d/Core/BaseObject.js
+++ src/sharp2d/Core/BaseObject.js
@@ -20,7 +20,7 @@ class BaseObject {
     error() {
         var message = Array.prototype.slice.call(arguments).join(" ");
         var error = this._name + " (" + this.constructor.name + "): " + message;
-        if (this._debug) {
+        if (BaseObject.debug) {
             console.log(error);
         }
         throw new Error(error);
@@ -30,44 +30,12 @@ class BaseObject {
         return Object.create(this);
     }
 
-    callEvent(eventName, ...args) {
-        if (this._enabled && eventName in this) {
-            this[eventName](eventName, ...args);
-        }
-    }
-
-    callEventEx(eventName, ...args) {
-        if (this._enabled && eventName in this) {
-            var before = "before" + this._ucFirst(eventName);
-            var after = "after" + this._ucFirst(eventName);
-            if(before in this && this[before](eventName, ...args)) {
-                this[eventName](eventName, ...args);
-                if(after in this) {
-                    this[after](eventName, ...args);
-                }
-            } else {
-                this[eventName](eventName, ...args);
-                if(after in this) {
-                    this[after](eventName, ...args);
-                }
-            }
-        }
-    }
-
     get name() {
         return this._name;
     }
 
     get game() {
         return BaseObject.prototype._gameInstance;
-    }
-
-    get debug() {
-        return BaseObject.prototype._debug;
-    }
-
-    set debug(value) {
-        BaseObject.prototype._debug = value;
     }
 
     get enable() {
@@ -82,8 +50,12 @@ class BaseObject {
         return this._enabled;
     }
 
-    _ucFirst(value) {
-        return value && value[0].toUpperCase() + value.slice(1);
+    static get debug() {
+        return BaseObject.prototype._debug;
+    }
+
+    static set debug(value) {
+        BaseObject.prototype._debug = value;
     }
 }
 
