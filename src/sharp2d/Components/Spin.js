2fee300be8f0e7e06d29177a967bf4f5a5307e66
Lealcy Tardelli Belegante <lealcy@gmail.com>
2016-03-04 13:56:00 -0300

    Make your entities spin...

---
+++ src/sharp2d/Components/Spin.js
@@ -0,0 +1,20 @@
+"use strict";
+
+class Spin extends Component {
+    constructor(name, angle) {
+        super(name);
+        this._angle = angle || 1;
+    }
+
+    beforeUpdate() {
+        this.entity.transform.rotate += this._angle;
+    }
+
+    get angle() {
+        return this._angle;
+    }
+
+    set angle(value) {
+        this._angle = value;
+    }
+}
