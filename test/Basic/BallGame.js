0e10993f685aa0a4484b4d71ffa24b5d91f82f60
Lealcy Tardelli Belegante <lealcy@gmail.com>
2016-03-10 17:33:08 -0300

    Remove test/Basic because its very outdated.

--- test/Basic/BallGame.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-class BallGame extends Game {
-    constructor(canvasElement) {
-        super(canvasElement);
-        this.ball = new Ball(this, "images/Soccer_Ball.png");
-    }
-}
